0effe993184f0e4cfad42760d24a9b9c7dc4352c
Rashad Gilliam <akklaim83@gmail.com>
2022-04-19 00:53:07 -0700

    changes

--- index.js
+++ index.js
@@ -6,23 +6,6 @@ let direction = null;
 let x = 100;
 let y = 250;
 
-function moveCharacter(){ 
-if (direction === 'west'){
-    x = x - 1
-}
-if (direction === 'north'){
-    y = y + 1
-}
-if (direction === 'east'){
-    x = x + 1
-}
-if (direction === 'south'){
-    y = y - 1
-}
-character.style.left = x + 'px'
-character.style.bottom = y + 'px'
-}
-
 setInterval(function() {
    
     if (direction === 'west'){
@@ -58,7 +41,7 @@ document.addEventListener('keydown', function(e){
     }
 })
 
-document.addEventListener('keyup', function(){
+document.addEventListener('keyup', function(e){
     direction = null
 })
 
@@ -81,6 +64,57 @@ function move(element) {
         moveWithArrowKeys: moveWithArrowKeys
     }
 }
+
+function moveWithArrowKeys(left, bottom) {
+    let direction = null;
+    let x = left;
+    let y = bottom;
+
+    element.style.left = x + 'px'
+    element.style.bottom = y + 'px'
+
+    function moveCharacter(){
+        if(direction ==='west'){
+            x-=1
+        }
+        if(direction === 'north'){
+            y+=1
+        }
+        if(direction === 'east'){
+            x+=1
+        }
+        if(direction === 'south'){
+            y-=1
+        }
+        elemment.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+    }
+
+    setInterval(moveCharacter, 1)
+
+    document.addEventListener('keydown', function (e){
+        if(e.repeat) return;
+
+        if(e.key === 'ArrowLeft'){
+            direction = 'west'
+        }
+        if(e.key === 'ArrowUp'){
+            direction = 'north'
+        }
+        if(e.key === 'ArrowRight'){
+            direction = 'east'
+        }
+        if(e.key === 'ArrowDown'){
+            direction = 'south'
+        }
+    })
+
+    document.addEventListener('keyup', function (e){
+        direction = null
+    })
+}
+
+// 5) Responding to a character's Direction change
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
